8a4dab9c99cbd874e14ef80fa913e1fb25546dd6
avash-12345 <aavashkunwar77@gmail.com>
2026-03-01 22:05:47 +0545

    api added

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -29,7 +29,7 @@ export const loginUserApi =(data) => Api.post("/api/auth/login",data)
 
 // ASSIGNMENTS
 export const addAssignment = (data) => 
-    Api.post("/api/assignments/add", data, config);
+    ApiFormData.post("/api/assignments/add", data, config);
 
 export const getAllAssignments = () => 
     Api.get("/api/assignments/all", config);
@@ -38,7 +38,12 @@ export const getAssignment = (id) =>
     Api.get(`/api/assignments/${id}`, config);
 
 export const updateAssignment = (id, data) => 
-    Api.put(`/api/assignments/${id}`, data, config);
+    ApiFormData.put(`/api/assignments/${id}`, data, config);
+
+export const downloadAssignment = (id)  => Api.get(`/api/assignments/download/${id}`, {
+    ...config,
+    responseType: "blob"
+});
 
 export const removeAssignment = (id) => 
     Api.delete(`/api/assignments/${id}`, config);
@@ -58,3 +63,15 @@ export const updateCourse = (id, data) =>
 
 export const removeCourse = (id) => 
     Api.delete(`/api/courses/${id}`, config);
+
+export const addResource    = (data)     => Api.post("/api/resources/add", data, config);
+export const getAllResources = ()         => Api.get("/api/resources/all", config);
+export const updateResource = (id, data) => Api.put(`/api/resources/update/${id}`, data, config);
+export const removeResource = (id)       => Api.delete(`/api/resources/delete/${id}`, config);
+
+export const getAllSubmissions           = ()          => Api.get("/api/assignment/submission/all", config);
+export const getSubmissionsByAssignment = (aid)       => Api.get(`/api/assignment/submission/${aid}`, config);
+export const reviewSubmission           = (id, data)  => Api.put(`/api/assignment/submission/review/${id}`, data, config);
+export const downloadSubmission         = (id)        => Api.get(`/api/assignment/submission/download/${id}`, { ...config, responseType: "blob" });
+export const submitAssignment           = (formData)  => Api.post("/api/assignment/submission/submit", formData, { headers: { ...config.headers, "Content-Type": "multipart/form-data" }});
+export const resubmitAssignment         = (id, formData) => Api.put(`/api/assignment/submission/resubmit/${id}`, formData, { headers: { ...config.headers, "Content-Type": "multipart/form-data" }});
